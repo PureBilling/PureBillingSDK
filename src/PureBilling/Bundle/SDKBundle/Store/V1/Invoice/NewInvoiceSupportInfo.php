54b576ad76e26673535ca7059a84dcda65b34eb3
Albert Lacarta <urodoz@gmail.com>
2014-02-28 10:04:48 +0000

    Added SupportInfo stores. refs #100

--- src/PureBilling/Bundle/SDKBundle/Store/V1/Invoice/NewInvoiceSupportInfo.php
+++ src/PureBilling/Bundle/SDKBundle/Store/V1/Invoice/NewInvoiceSupportInfo.php
@@ -3,31 +3,21 @@
 namespace PureBilling\Bundle\SDKBundle\Store\V1\Invoice;
 
 use Symfony\Component\Validator\Constraints as Assert;
+use PureBilling\Bundle\SDKBundle\Store\Base\SupportInfoBase;
 use PureMachine\Bundle\SDKBundle\Store\Annotation as Store;
-use PureBilling\Bundle\SDKBundle\Constraints as PBAssert;
 
-class InvoiceSupportInfo extends NewInvoiceSupportInfo
+class NewInvoiceSupportInfo extends SupportInfoBase
 {
-
-    /**
-     * @Store\Property(description="id of the support info item")
-     * @PBAssert\Type(type="id", idPrefixes={"invoicesupport"})
-     * @Store\Entity()
-     * @Assert\NotBlank()
-     */
-    protected $id;
 
     /**
      * @Store\Property(description="delivery URL if any")
      * @Assert\Type("string")
-     * @Store\EntityMapping("deliveryUrl")
      */
     protected $deliveryUrl;
 
     /**
      * @Store\Property(description="Invoice public key")
      * @Assert\Type("string")
-     * @Store\EntityMapping("invoiceTransaction.publicKey")
      * @Assert\NotBlank()
      */
     protected $invoice;
