cc300ac8a929361213f8f8536e4008abdceef669
Arnaud Bonnevigne <arnaud.bonnevigne@wengo.com>
2014-04-10 17:41:39 +0200

    fix registerAnnotations autoload

--- autoloader.php
+++ autoloader.php
@@ -134,9 +134,15 @@ class SplClassLoader_PureBillingRawPHP
     }
 }
 
+/**
+ * Try to register annotations
+ */
 if (file_exists(__DIR__.'/vendor/autoload.php')) {
     require_once(__DIR__.'/vendor/autoload.php');
     registerAnnotations(__DIR__.'/vendor');
+} elseif(file_exists(__DIR__.'/../../../vendor/autoload.php')) {
+    require_once(__DIR__.'/../../../vendor/autoload.php');
+    registerAnnotations(__DIR__.'/../../../vendor');
 }
 
 function registerAnnotations($composerPath) {
