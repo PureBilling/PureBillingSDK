45042b9d7b092c1a1482c969f4ba7cf0e6884eba
Lionel Martin <lm@purebilling.com>
2014-05-28 18:39:30 +0000

    Update invoice Getter

--- src/PureBilling/Bundle/SDKBundle/Store/V1/Charge/Action/Get.php
+++ src/PureBilling/Bundle/SDKBundle/Store/V1/Charge/Action/Get.php
@@ -20,7 +20,7 @@ class Get extends ExpandableAction
      * @Store\Property(description="Some properties return a ID. If you want a full object, add the property path here")
      * @Assert\Type("array")
      * @Assert\Choice(multiple=true, choices={"customer",
-     *                                        "invoice",
+     *                                        "invoice", "invoice.subscriptionInfo", "invoice.supportInfo",
      *                                        "updateNotifications", "origin", "paymentMethod"})
      */
     protected $propertiesToExpand = array();
