4f90e9a7f421e0ae82d6d35e8219f2912669cf28
Lionel Martin <lm@purebilling.com>
2014-07-22 16:03:45 +0000

    Allow partial refund

--- src/PureBilling/Bundle/SDKBundle/Store/V1/Charge/Action/Refund.php
+++ src/PureBilling/Bundle/SDKBundle/Store/V1/Charge/Action/Refund.php
@@ -4,8 +4,35 @@ namespace PureBilling\Bundle\SDKBundle\Store\V1\Charge\Action;
 
 use PureBilling\Bundle\SDKBundle\Store\Base\Action;
 use PureMachine\Bundle\SDKBundle\Store\Annotation as Store;
+use PureBilling\Bundle\SDKBundle\Constraints as PBAssert;
+use Symfony\Component\Validator\Constraints as Assert;
 
-class Refund extends Rebill
+class Refund extends Action
 {
+    /**
+     * @Store\Property(description="billing Transaction to refund")
+     * @PBAssert\Type(type="id", idPrefixes={"billing"})
+     * @Assert\NotNull()
+     */
 
+    protected $billingTransaction;
+
+    /**
+     * @Store\Property(description="External id")
+     * @Assert\Type("string")
+     */
+    protected $externalId;
+
+    /**
+     * @Store\Property(description="Short description of billed item")
+     * @Assert\Type("string")
+     */
+    protected $shortDescription;
+
+    /**
+     * @Store\Property(description="amount to refund, NULL if same a $billingTransaction")
+     * @Assert\Type("float")
+     * @Assert\GreaterThan(0)
+     */
+    protected $amount;
 }
