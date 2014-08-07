99675c1c09b15af734e9a60c604dd21f524e7404
Albert Lacarta <urodoz@gmail.com>
2014-08-07 15:13:15 +0000

    Added SEPA to form store. refs #336

--- src/PureBilling/Bundle/SDKBundle/Store/V1/PaymentMethod/NewPaymentForm.php
+++ src/PureBilling/Bundle/SDKBundle/Store/V1/PaymentMethod/NewPaymentForm.php
@@ -8,10 +8,21 @@ use PureMachine\Bundle\SDKBundle\Store\Annotation as Store;
 
 class NewPaymentForm extends Element
 {
+    const TYPE_CREDITCARD = "Creditcard";
+    const TYPE_IBAN = "IBAN";
+
     /**
      * @Store\Property(description="callback we will call after creditcard from")
      * @Assert\Type("string")
      * @Assert\NotBlank
      */
     protected $callback;
+
+    /**
+     * @Store\Property(description="Form type")
+     * @Assert\Type("string")
+     * @Assert\Choice({"Creditcard", "IBAN"})
+     * @Assert\NotBlank()
+     */
+    protected $type='Creditcard';
 }
