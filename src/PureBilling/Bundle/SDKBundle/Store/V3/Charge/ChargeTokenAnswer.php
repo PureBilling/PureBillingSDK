1e71904850ae5eab6790be159be847d4e5c19a80
Lionel Martin <lm@purebilling.com>
2016-01-18 12:49:36 +0100

    Remove status in ChargeTokenAnswer V3

--- src/PureBilling/Bundle/SDKBundle/Store/V3/Charge/ChargeTokenAnswer.php
+++ src/PureBilling/Bundle/SDKBundle/Store/V3/Charge/ChargeTokenAnswer.php
@@ -17,27 +17,12 @@ class ChargeTokenAnswer extends BaseStoreV3
     protected $billingTransaction;
 
     /**
-     * @Store\Property(description="billing transaction id")
-     * @PBAssert\Type(type="id", idPrefixes={"billing"})
-     * @Assert\NotBlank()
-     */
-    protected $id;
-
-    /**
      * @Store\Property(description="billing transaction type")
      * @Assert\Type("string")
      * @Assert\Choice({"capture", "authorize", "refund"})
      * @Assert\NotBlank()
      */
     protected $billingTransactionType;
-
-    /**
-     * @Store\Property(description="billing transaction status")
-     * @Assert\Type("string")
-     * @Assert\Choice({"running", "paid", "unpaid"})
-     * @Assert\NotBlank()
-     */
-    protected $status;
 
     /**
      * @Store\Property(description="billing transaction status")
